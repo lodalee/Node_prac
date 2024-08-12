2e1c112fb2012063a65cfc6e856460347aaf27e7
lodalee <cwlee4633@naver.com>
2024-08-12 13:29:38 +0900

    NestJS와 DB연결하기, 환경 변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { CatsService } from './cats/cats.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    CatsModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService, CatsService],
 })
