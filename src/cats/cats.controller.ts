cfc097e1a986eacba74711d0a15a0ccf1c81a366
lodalee <cwlee4633@naver.com>
2024-08-12 12:46:43 +0900

    exception filter & pipes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,34 @@
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { PositiveIntPipe } from 'src/common/pipes/positiveIntPipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getAllCat() {
+    throw new HttpException('api broken', 401);
     return 'all cat';
   }
 
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    // console.log(param);
+    // console.log(typeof param);
     return 'one cat';
   }
 
---
+++ src/common/pipes/positiveIntPipe.ts
@@ -0,0 +1,9 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: number) {
+    console.log(value);
+    return value;
+  }
+}
